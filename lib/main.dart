42074589e349269bcf321ab2a2cb2b7ec1d2d9a6
mrrapp <rappjeremiahn@gmail.com>
2023-06-09 00:02:21 +0100

    Added themes to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:my_expenses/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
 void main() {
   runApp(
-     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home:const Expenses(),
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+      ),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+          // actionsIconTheme: const IconThemeData().copyWith(
+          //   color: kColorScheme.onSecondary,
+          // ),
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 24,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.secondaryContainer,
+          ),
+        ),
+        textTheme: const TextTheme().copyWith(
+          titleLarge: TextStyle(
+            fontSize: 18,
+            color: kColorScheme.secondary,
+          ),
+          bodyMedium: TextStyle(
+            color: kColorScheme.onPrimaryContainer,
+          ),
+          bodySmall: const TextStyle(
+            color: Colors.black,
+          ),
+          displaySmall: const TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      themeMode: ThemeMode.system,
+      home: const Expenses(),
     ),
   );
 }
